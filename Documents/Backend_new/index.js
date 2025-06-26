9f833e0fa974dc84a8b3f127077c94e8f2d7edde
hemaprabhag <hemaprabha034@gmail.com>
2025-06-26 13:29:17 +0530

    added

---
+++ Documents/Backend_new/index.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+app.use(express.json())
+require('dotenv').config();
+const cors = require('cors');
+app.use(cors());
+app.use(express.urlencoded({extended: true}));
+const PORT= 4000;
+const MONGO_URI="mongodb+srv://HemaPrabha:Hema@cluster0.txecrgd.mongodb.net/expensetracker?retryWrites=true&w=majority&appName=Cluster0"
+const expenseSchema = new mongoose.Schema({
+    title:{
+        type:String,
+        required: true
+
+    },
+    amount:{ 
+        type: Number,
+        required: true
+    }
+})
+
+const Expense = mongoose.model('Expense',expenseSchema);
+mongoose.connect(MONGO_URI)
+.then(()=>{
+    console.log("Connected to mongoDb")
+})
+.catch((err)=>{
+    console.error('mongoDb connection error :',err)
+})
+
+app.post('/Expense',async (req, res)=>{
+    try{
+        const { title,amount} = req.body;
+        const expense = new Expense({title , amount})
+        await expense.save()
+        res.status(201).json(expense)
+    }catch (error) {
+         console.error('Error saving expense ',error)
+         res.status(500).json({error: "failed to save"})
+    }
+})
+app.listen(PORT,()=>{
+    console.log(`server is listening on port ${PORT}`)
+})
+
+app.get('/Expense',async (req,res)=>{
+    try{
+        const expenses=await Expense.find();
+        res.json(expenses)
+    }catch(error){
+        console.error('Error getting expense',error)
+        res.status(500).json({error:"Failed to fetch"})
+    }
+})
+
+app.delete('/expense/:userID', async(req,res)=>{
+    try{
+        const{ userID } = req.params;
+    const deleteExpense = await Expense.findByIdAndDelete(userID)
+    if(!deleteExpense){
+        return res.status(404).json({error :"not found"});
+    }
+    res.status(201).json({message: "expense deleted" , deleteExpense})
+    }
+    catch(error){
+        res.status(500).json({error:'failed to delete '})
+    }
+})
